6fe1f7caad73dab60941709686fbcb0bdf3ab40c
Ori Hoch <ori@uumpa.com>
2022-11-28 08:28:59 +0200

    Sync from Azure DevOps repository

--- set_keyvault_secrets.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -z "$KEYVAULT_UUMPA_ROOT_DOMAIN" ]; then exit 1; fi
-if [ -z "$KEYVAULT_ADMIN_EMAIL" ]; then exit 1; fi
-
-az keyvault secret set --name "uumpa-root-domain" --vault-name "${1}" --value "${KEYVAULT_UUMPA_ROOT_DOMAIN}" &&\
-az keyvault secret set --name "admin-email" --vault-name "${1}" --value "${KEYVAULT_ADMIN_EMAIL}"
--- terraform_apply.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-./write_tfvars.py &&\
-terraform apply "$@" &&\
-./set_kubernetes_context.py &&\
-./save_terraform_outputs.py
