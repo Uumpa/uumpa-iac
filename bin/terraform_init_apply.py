c7e2f60c0ca6dbe911ac823a042974855a7d451c
sync_to_github <sync_to_github@uumpa-azure>
2022-11-29 20:31:54 +0000

    Sync from Azure DevOps repository

--- bin/terraform_init_apply.py
+++ bin/terraform_init_apply.py
@@ -8,7 +8,7 @@ import glob
 HELP = '''
 Iterate over all the environment modules and run terraform init and apply for each one.
 
-Usage: bin/terraform_init_apply.py <environment_name> [--from=<module>] [--to=<module>] [--dry-run] [--help] [apply_args..]
+Usage: bin/terraform_init_apply.py <environment_name> [--from=<module>] [--to=<module>] [--plan] [--dry-run] [--only-init] [--help] [apply_args..]
 '''.strip()
 
 CORE_MODULES = [
@@ -21,7 +21,7 @@ CORE_MODULES = [
 
 
 def parse_args(args):
-    apply_args, from_module, to_module, dry_run, show_help = [], None, None, False, False
+    apply_args, from_module, to_module, dry_run, show_help, only_init, plan = [], None, None, False, False, False, False
     for arg in args:
         if arg.startswith('--to='):
             to_module = arg.split('=')[1]
@@ -31,9 +31,13 @@ def parse_args(args):
             dry_run = True
         elif arg == '--help':
             show_help = True
+        elif arg == '--only-init':
+            only_init = True
+        elif arg == '--plan':
+            plan = True
         else:
             apply_args.append(arg)
-    return apply_args, from_module, to_module, dry_run, show_help
+    return apply_args, from_module, to_module, dry_run, show_help, only_init, plan
 
 
 def get_environment_modules(environment_name):
@@ -44,19 +48,22 @@ def get_environment_modules(environment_name):
     return environment_modules
 
 
-def process_module(environment_name, module, apply_args, dry_run):
-    print(f'Running terraform init and apply for {environment_name}/{module} with apply args: {apply_args}')
+def process_module(environment_name, module, apply_args, dry_run, only_init, plan):
+    plan_apply = "plan" if plan else "apply"
+    what = 'init' if only_init else f'init and {plan_apply}'
+    print(f'Running terraform {what} for {environment_name}/{module} with {plan_apply} args: {apply_args}')
     if not dry_run:
         subprocess.check_call([
             'bin/terraform.py', environment_name, module, 'init'
         ])
-        subprocess.check_call([
-            'bin/terraform.py', environment_name, module, 'apply', *apply_args
-        ])
+        if not only_init:
+            subprocess.check_call([
+                'bin/terraform.py', environment_name, module, plan_apply, *apply_args
+            ])
 
 
 def main(environment_name, *args):
-    apply_args, from_module, to_module, dry_run, show_help = parse_args(args)
+    apply_args, from_module, to_module, dry_run, show_help, only_init, plan = parse_args(args)
     if show_help:
         print(HELP)
         exit(1)
@@ -69,7 +76,7 @@ def main(environment_name, *args):
             got_from_module = True
         if not got_from_module:
             continue
-        process_module(environment_name, module, apply_args, dry_run)
+        process_module(environment_name, module, apply_args, dry_run, only_init, plan)
         if module == to_module:
             break
 
