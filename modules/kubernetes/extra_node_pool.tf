11c6e8ea34bfa4894279d7663866ce9350ca525c
sync_to_github <sync_to_github@uumpa-azure>
2022-12-19 11:55:51 +0000

    Sync from Azure DevOps repository

--- modules/kubernetes/extra_node_pool.tf
+++ modules/kubernetes/extra_node_pool.tf
@@ -3,9 +3,9 @@ resource "azurerm_kubernetes_cluster_node_pool" "extra" {
   name = "extra"
   vm_size = "Standard_B2s"
   node_count          = 0
-  enable_auto_scaling = true
-  min_count           = 0
-  max_count           = 3
+  enable_auto_scaling = false
+#  min_count           = 0
+#  max_count           = 3
   os_disk_type        = "Managed"
   os_disk_size_gb     = 100
   os_sku              = "Ubuntu"
